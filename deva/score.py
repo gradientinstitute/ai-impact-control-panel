52fac12b71a5a3f8a7031aff790aa913f7d5c7a7
socallaghan <simonsemailaddress@gmail.com>
2021-08-24 09:39:09 +1000

    Add false postives and false negatives as metrics

--- deva/score.py
+++ deva/score.py
@@ -7,19 +7,28 @@ def model_auc(y_test, y_pred):
     auc = metrics.auc(fpr, tpr)
     return auc
 
-
 def model_acc(y_test, y_pred):
     fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred, pos_label=1)
     acc = np.mean(y_pred == y_test)
     return acc
 
+def fn(y_test, y_pred):
+    tn, fp, fn, tp = metrics.confusion_matrix(y_test, y_pred).ravel()
+    return fn
 
-# A dictionary that maps config dictionary metric headings to functions
+def fp(y_test, y_pred):
+    tn, fp, fn, tp = metrics.confusion_matrix(y_test, y_pred).ravel()
+    return fp
+
+# A dictionary that maps config dictionary metric headings to functions.
+# The key must match the heading in the model toml
 # First element of the value tuple is a function to calculate the score.
 # Second element of the value tuple is the optimal value of that score.
 metrics_dict = {
         'accuracy': {'func': model_acc, 'optimal': 1.},
         'auc': {'func': model_auc, 'optimal': 1.},
+        'fp': {'func': fp, 'optimal': 0.},
+        'fn': {'func': fn, 'optimal': 0.},
         }
 
 
