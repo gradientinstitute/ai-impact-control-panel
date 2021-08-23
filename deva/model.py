9e1d9e1a3fb2c323634dc3332e3242380be14821
Dan Steinberg <dan.m.steinberg@gmail.com>
2021-08-23 17:27:44 +1000

    initial threshold config, needs testing

--- deva/model.py
+++ deva/model.py
@@ -1,4 +1,5 @@
 import itertools
+import warnings
 import numpy as np
 
 from sklearn.ensemble import RandomForestClassifier
@@ -20,6 +21,32 @@ def translate_config(cfg):
         d = {0: 1, 1: w}
         out['class_weight'] = d
     return out
+
+
+def apply_threshold(threshold, y_scores, X):
+    '''apply binary threshold(s) to a score.'''
+    if isinstance(threshold, float):
+        y_pred = (y_scores > threshold).astype(int)
+        return y_pred
+
+    if len(threshold) > 1:
+        warnings.warn('Only one element of the threshold dictionary will be'
+                      'used.', UserWarning)
+
+    sens_attr, threshdict = next(iter(threshold.items()))
+    y_pred = np.zeros(len(y_scores), dtype=int)
+
+    # NOTE: This does not check for missing classes in the TOML spec. These
+    # will automatically be classified as 0. For examples, if we have multiple
+    # gender classes, but only a subset are in the TOML config, the others will
+    # not be classified (but they will have a score).
+    for cls, thresh in threshdict.items():
+        if cls.isnumeric():  # work around for non-numeric toml keys
+            cls = float(cls)
+        cls_ind = X.loc[:, sens_attr] == cls
+        y_pred[cls_ind] = y_scores[cls_ind] > thresh
+
+    return y_pred
 
 
 def iterate_hypers(base_cfg, range_cfg, list_cfg, instance_cfg, n_range_draws):
@@ -68,10 +95,14 @@ def iter_models(X_train, y_train, t_train, X_test, y_test, t_test, cfg):
                            instance_cfg, cfg['n_range_draws'])
     for param_name, param_dict in piter:
         processed_params = translate_config(param_dict)
+        threshold = processed_params.pop('threshold', None)
         m = model_dict[model_str](**processed_params)
         m.fit(X_train, y_train)
-        y_pred = m.predict(X_test)
-        y_scores = m.predict_proba(X_test)[:, 1]
+        y_scores = m.predict_proba(X_test)[:, 1]  # NOTE: assumes binary class.
+        if threshold is None:
+            y_pred = m.predict(X_test)
+        else:
+            y_pred = apply_threshold(threshold, y_scores, X_test)
         model_scores = score_model(y_pred, y_scores, y_test, X_test,
                                    metrics_cfg)
         d = {
