b5ff4dbce44be22517b67d8f1c326eccc49df2c2
Lachlan McCalman <lachlan@mccalman.info>
2021-09-06 08:21:51 +1000

    slightly less terrible layout

--- server/deva-ts/src/App.tsx
+++ server/deva-ts/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App container mx-auto text-center">
-          <h1>AI Governance Control Panel</h1>
+          <h1 className="pb-8">AI Governance Control Panel</h1>
           <PairwiseComparator />
     </div>
   );
@@ -22,7 +22,7 @@ function PairwiseComparator() {
   const [models, setModels] = useState<ModelChoice>({ model1: "", model2: ""});
   const [modelChosen, setModelChosen] = useState("");
   useEffect(() => {
-    let req = modelChosen == "" ? "choice" : "choice/" + modelChosen;
+    let req = modelChosen === "" ? "choice" : "choice/" + modelChosen;
     async function fetchData() {
       const result = await axios.get<ModelChoice>(req)
       setModels(result.data);
@@ -31,24 +31,36 @@ function PairwiseComparator() {
     }, [modelChosen]);
   
   return (
-    <div>
-      <ModelView model={models.model1}/>
-      <ModelView model={models.model2}/>
-      <ButtonInterface label="model1" onClick = { () => {setModelChosen("1")} } />
-      <ButtonInterface label="model2" onClick = { () => {setModelChosen("2")} } />
+    <div className="flex flex-row gap-5">
+      <div className="flex-1 space-y-5">
+        <ModelView model={models.model1}/>
+        <ButtonInterface label="model1" onClick = { () => {setModelChosen("1")} } />
+      </div>
+      <div className="flex-1 space-y-5">
+        <ModelView model={models.model2}/>
+        <ButtonInterface label="model2" onClick = { () => {setModelChosen("2")} } />
+      </div>
     </div>
   )
 }
 
 function ModelView(props) {
-  return <p>I am a picture of model {props.model}</p>
+  return (
+    <div className="h-48 bg-gray-50 flex flex-wrap content-center justify-center">
+      <div>
+      <p>I am a picture of model {props.model}</p>
+      </div>
+    </div>
+    )
 }
 
 function ButtonInterface(props) {
   return (
     <div>
-      <button className="bg-green-500" onClick={() => props.onClick()}>
-        {props.label}
+      <button className="bg-green-500 rounded-lg" onClick={() => props.onClick()}>
+        <div className="p-4">
+          {props.label}
+        </div>
       </button>
     </div>
   );
