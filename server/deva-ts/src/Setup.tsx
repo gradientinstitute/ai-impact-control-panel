5284d7fa6ed39c2fce1bb03f3e98d411376035dd
Ri Liu <me@ri.id.au>
2021-12-02 16:05:29 +1100

    reorganise components

--- server/deva-ts/src/Setup.tsx
+++ server/deva-ts/src/Setup.tsx
@@ -52,12 +52,9 @@ export function SetupPane({}) {
   );
 }
 
-// each step of intro flow
+// steps of intro flow
 function Steps({}) {
   const [stepIndex, setStepIndex] = useState(0);
-  const [_pane, setPane] = useRecoilState(paneState);
-  const current = useRecoilValue(currentScenarioState);
-  const [_scenario, setScenario] = useRecoilState(scenarioState);
 
   function handleStepsChange(i) {
     setStepIndex(i);
@@ -66,34 +63,53 @@ function Steps({}) {
  return (
    <Tabs className="intro-content flex items-stretch" index={stepIndex} onChange={handleStepsChange}>
      <TabPanels className="self-center flex-1">
-       <TabPanel key={0}>
-          <p className="text-lg">I want to elicit</p>
-          <StartButtons onChange={handleStepsChange} />
-       </TabPanel>
-       <TabPanel key={1}>
-        <Summary />
-        <div className="flex justify-between btn-row mt-12">
-          <div className="flex flex-1 align-middle text-left">
-            <button className="hover:text-gray-300 transition"
-              onClick={() => stepIndex >=0 && setStepIndex(stepIndex-1)}>
-              &#8249; Back
-            </button>
-          </div>
-          <button className="btn text-xl uppercase py-4 px-8 font-bold rounded-lg"
-            onClick={() => {
-              if (current) {
-                setScenario(current)
-                setPane(Pane.Intro);
-              }
-            }}
-            disabled={current === null}>
-            Start
-          </button>
-        </div>
-       </TabPanel>
+       <Step1 handleStepsChange={handleStepsChange} />
+       <Step2 stepIndex={stepIndex} setStepIndex={setStepIndex}/>
      </TabPanels>
    </Tabs>
  )
+}
+
+// first step: choose what to elicit
+function Step1({handleStepsChange}) {
+  return (
+    <TabPanel key={0}>
+      <p className="text-lg">I want to elicit</p>
+      <StartButtons onChange={handleStepsChange} />
+  </TabPanel>
+  )
+}
+
+// second step: select scenario
+function Step2({stepIndex, setStepIndex}) {
+  const [_pane, setPane] = useRecoilState(paneState);
+  const current = useRecoilValue(currentScenarioState);
+  const [_scenario, setScenario] = useRecoilState(scenarioState);
+
+  return (
+    <TabPanel key={1}>
+      <p className="text-lg pb-6">Select a scenario</p>
+      <ScenarioSelector />
+      <div className="flex justify-between btn-row mt-12">
+        <div className="flex flex-1 align-middle text-left">
+          <button className="hover:text-gray-300 transition"
+            onClick={() => stepIndex >=0 && setStepIndex(stepIndex-1)}>
+            &#8249; Back
+          </button>
+        </div>
+        <button className="btn text-xl uppercase py-4 px-8 font-bold rounded-lg"
+          onClick={() => {
+            if (current) {
+              setScenario(current)
+              setPane(Pane.Intro);
+            }
+          }}
+          disabled={current === null}>
+          Start
+        </button>
+      </div>
+    </TabPanel>
+  )
 }
 
 // Dropdown box for selecting a scenario
@@ -104,10 +120,37 @@ function ScenarioSelector({}) {
   const entries = Object.entries(scenarios)
   const [tabIndex, setTabIndex] = useState(-1);
   const [currentIndex, setCurrentIndex] = useState(-1)
+  const tabActive = tabIndex === currentIndex && currentIndex > -1
 
   function handleTabsChange(i) {
     setTabIndex(i);
   }
+
+  const tabs = entries.map(([name, v], i) => {
+    return (
+      <Tab
+        data-current = { current === name }
+        key={i}
+        onMouseEnter={() => setTabIndex(i)}
+        onMouseLeave={() => current ? setTabIndex(currentIndex) : setTabIndex(-1)}
+        onMouseDown={() => {
+          setCurrent(name);
+          setCurrentIndex(i);
+        }}
+      >
+        {v.name}
+      </Tab>
+    )
+  })
+
+  const tabPanels = entries.map(([name, v], i) => {
+    return (
+      <TabPanel key={i}>
+        <span className="pr-6"><strong>Objectives:</strong> {Object.keys(v.objectives).length}</span>
+        <span><strong>Metrics:</strong> {Object.keys(v.metrics).length}</span>
+        <p className="pt-3">{v.operation}</p>
+      </TabPanel>
+    )})
   
   return (
     <Tabs
@@ -117,46 +160,16 @@ function ScenarioSelector({}) {
       onChange={handleTabsChange}
     >
       <TabList className="scenario-list text-left col-span-1 font-bold">
-      { entries.map(([name, v], i) => {
-        return (
-          <Tab
-            data-current = { current === name }
-            key={i}
-            onMouseEnter={() => setTabIndex(i)}
-            onMouseLeave={() => current ? setTabIndex(currentIndex) : setTabIndex(-1)}
-            onMouseDown={() => {
-              setCurrent(name);
-              setCurrentIndex(i);
-            }}
-          >
-            {v.name}
-          </Tab>
-        )
-      })}
-    </TabList>
-      <TabPanels className="col-span-2 text-left py-3 px-6" data-active={tabIndex === currentIndex && currentIndex > -1}>
-        { entries.map(([name, v], i) => {
-          return (
-          <TabPanel key={i}>
-            <span className="pr-6"><strong>Objectives:</strong> {Object.keys(v.objectives).length}</span>
-            <span><strong>Metrics:</strong> {Object.keys(v.metrics).length}</span>
-            <p className="pt-3">{v.operation}</p>
-          </TabPanel>)
-        })}
+        { tabs }
+      </TabList>
+      <TabPanels
+        className="col-span-2 text-left py-3 px-6"
+        data-active={tabActive}
+      >
+        { tabPanels }
       </TabPanels>
     </Tabs>
     );
-}
-
-// Summary of the currently selected scenario
-function Summary({}) {
-  
-  return (
-    <div>
-      <p className="text-lg pb-6">Select a scenario</p>
-        <ScenarioSelector />
-    </div>
-  );
 }
 
 // select the type of elicitation to do with two buttons
