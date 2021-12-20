cebfacf0b2a203f892bdff0b35eb8a43f78f0f84
adaluong <35322993+adaluong@users.noreply.github.com>
2021-12-20 13:17:13 +1100

    change range scrollbars to one-way sliders

--- server/deva-ts/src/Constrain.tsx
+++ server/deva-ts/src/Constrain.tsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react';
-import { Range } from 'rc-slider';
+import Slider, {Range} from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import ReactSlider from 'react-slider'
 import { atom, selector, useRecoilState, useRecoilValue } from 'recoil';
@@ -12,6 +12,11 @@ export const allCandidatesState = atom({
   key: 'allCandidates', 
   default: null, 
 });
+
+export const blockedState = atom({
+  key: 'uniqueKey', // TODO
+  default: null,
+})
 
 // maximum possible ranges (doesnt change)
 export const maxRangesState = selector({
@@ -83,7 +88,7 @@ export function ConstraintPane({}) {
 
   // set initial value of the constraints
   useEffect(() => {
-    setConstraints(maxRanges);
+    setConstraints(maxRanges)
   }, [maxRanges]);
 
       
@@ -166,23 +171,22 @@ function QuantitativeConstraint({x, maxRanges, constraints, uid}) {
   const vals = maxRanges[uid];
   const min = vals[0];
   const max = vals[1];
-  const cmin = constraints[uid][0];
-  const cmax = constraints[uid][1];
-  const cstring = u.prefix + " (" + cmin + " - " + cmax + ")\n" + u.suffix;
   const min_string = u.prefix + " " + min + " " + u.suffix;
   const max_string = u.prefix + " " + max + " " + u.suffix;
   const name = u.name;
+  const cmin = constraints[uid][0];
+  const cmax = constraints[uid][1];
+  const cstring = u.prefix + " (" + cmin + " - " + cmax + ")\n" + u.suffix;
 
   return (
     <div key={uid} className="grid grid-cols-5 gap-8 bg-gray-600 rounded-lg p-4">
       
       <h2 className="col-span-5 text-center">{name}</h2>
-      
       <p className="col-span-5 text-3xl">{cstring}</p>
 
       <p className="col-span-1 text-xs text-right my-auto">{min_string}</p>
       <div className="col-span-3 my-auto">
-        <RangeConstraint uid={uid} min={min} max={max} marks={null}/>
+        <RangeConstraint uid={uid} min={min} max={max} marks={null} higherIsBetter={u.higherIsBetter}/>
       </div>
       <p className="col-span-1 text-xs text-left my-auto">{max_string}</p>
     </div>
@@ -212,33 +216,74 @@ function QualitativeConstraint({x, maxRanges, constraints, uid}) {
   .map(x => (<p>{x}</p>));
 
   return (
-    <div key={uid} className="grid grid-cols-6 gap-8 bg-gray-600 rounded-lg my-auto">
+    <div key={uid} className="grid grid-cols-10 gap-8 bg-gray-600 rounded-lg p-4">
             
-      <h2 className="col-span-6 text-center">{name}</h2>
+      <h2 className="col-span-10 text-center">{name}</h2>
 
-      <p className="col-span-2 my-auto">{}</p>
-      <div className="col-span-1 text-center">
+      <p className="col-span-3 my-auto">{}</p>
+      <div className="col-span-2 text-center">
       <p className="font-bold">Allowed</p>
       <p>{allowed}</p>
       </div>
 
-      <div className="col-span-1 text-center">
+      <div className="col-span-2 text-center">
       <p className="font-bold">Not Allowed</p>
       <p>{notAllowed}</p>
       </div>
-      <p className="col-span-2 my-auto">{}</p>
+      <p className="col-span-3 my-auto">{}</p>
 
-      <p className="col-span-1 my-auto">{}</p>
-      <div className="col-span-4 my-auto">
-        <RangeConstraint uid={uid} min={min} max={max} marks={options}/>
+      <p className="col-span-2 my-auto">{}</p>
+      <div className="col-span-6 my-auto">
+        <RangeConstraint uid={uid} min={min} max={max} marks={options} higherIsBetter={u.higherIsBetter}/>
       </div>
-      <p className="col-span-1 my-auto">{}</p>
+      <p className="col-span-2 my-auto">{}</p>
 
     </div>
   )
 }
 
-function RangeConstraint2({uid, min, max}) {
+function RangeConstraint({uid, min, max, marks, higherIsBetter}) {
+  const [constraints, setConstraints] = useRecoilState(constraintsState);
+  const val = higherIsBetter ? constraints[uid][0] : constraints[uid][1];
+  const all = useRecoilValue(allCandidatesState);
+  // const colours = useRecoilValue(); // TODO
+
+  function onChange(x: any) {
+    let n = {...constraints}
+    // modify either the lower bound or higher bound
+    n[uid] = higherIsBetter ? [x, n[uid][1]] : [n[uid][0], x];
+    const withNew = filterCandidates(all, n);
+    if (withNew.length > 0) {
+      setConstraints(n)
+    }
+  }
+  
+  let rangeProps = {
+      min: min,
+      max: max,
+      onChange: onChange,
+      allowCross: false,
+      value: val,
+      step: 1,
+      handleStyle: {backgroundColor: "gray"},
+      trackStyle: higherIsBetter ? {backgroundColor: "green"} : {backgroundColor: "red"},
+      railStyle: higherIsBetter ? {backgroundColor: "red"} : {backgroundColor: "green"},
+      // ...Style : state associated with recoil
+    };
+  
+  if (marks !== null) {
+    rangeProps["marks"] = marks;
+    rangeProps["step"] = null;
+  }
+
+  return (
+  <div>
+    <Slider {...rangeProps} />
+  </div>
+  );
+}
+
+function RangeConstraint2({uid, min, max, marks}) {
   
   const [constraints, setConstraints] = useRecoilState(constraintsState);
   const value = constraints[uid];
@@ -274,7 +319,7 @@ function RangeConstraint2({uid, min, max}) {
   );
 }
 
-function RangeConstraint({uid, min, max, marks}) {
+function RangeConstraint3({uid, min, max, marks}) {
   const [constraints, setConstraints] = useRecoilState(constraintsState);
   const val = constraints[uid];
   const all = useRecoilValue(allCandidatesState);
@@ -293,7 +338,7 @@ function RangeConstraint({uid, min, max, marks}) {
       max: max,
       onChange: onChange,
       allowCross: false,
-      value: [...val],
+      value: [...val]
     };
   
   if (marks !== null) {
