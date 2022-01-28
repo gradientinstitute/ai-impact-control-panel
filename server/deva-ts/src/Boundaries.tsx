c3ff1e627d13ac1638976132b515ec43f5fe00eb
Alistair Reid <alistair.reid@gradientinstitute.org>
2022-01-28 16:37:13 +1100

    Create boundaries panel path

---
+++ server/deva-ts/src/Boundaries.tsx
@@ -0,0 +1,9 @@
+export function BoundariesPane({}) {
+
+  return (
+    <div className="mx-auto max-w-screen-2xl grid gap-x-8 gap-y-10 grid-cols-1 text-center items-center pb-10">
+      <h1>Boundaries Placeholder</h1>
+      Your content here. We may need to fix the step numbering above.
+    </div>
+  );
+}
