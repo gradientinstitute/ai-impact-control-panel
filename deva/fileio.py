e536478ada3db0c43435b8101b79fd52b8bcce14
Cindy Chen <u6827101@anu.edu.au>
2022-01-31 07:55:52 +0800

    change to pytest

--- deva/fileio.py
+++ deva/fileio.py
@@ -126,20 +126,21 @@ def load_all_metrics(metrics, candidates):
         metrics[u]["min"] = min(c[u] for c in candidates)
 
         # set min/max range default using nice_range
-        if metrics[u]["isMetrics"]:
-            if "min_range" not in metrics[u] or "max_range" not in metrics[u]:
-                (metrics[u]["range_min"],
-                 metrics[u]["range_max"]) = nice_range(metrics[u]["min"],
-                                                       metrics[u]["max"])
+        if metrics[u]["isMetric"]:
+            (range_min, range_max) = nice_range(metrics[u]["min"],
+                                                metrics[u]["max"])
+            if "range_min" not in metrics[u] and "range_max" not in metrics[u]:
+                metrics[u]["range_min"] = range_min
+                metrics[u]["range_max"] = range_max
+            elif "range_min" not in metrics[u]:
+                metrics[u]["range_min"] = range_min
+            elif "range_max" not in metrics[u]:
+                metrics[u]["range_max"] = range_max
 
         if metrics[u]["type"] == "qualitative":
             load_qualitative_metric(metrics, candidates, u)
         elif metrics[u]["type"] == "quantitative":
             load_quantitative_metric(metrics, candidates, u)
-        # if metrics[u]["type"] == "qualitative":
-        #     load_qualitative_metric(metrics, candidates, u)
-        # elif metrics[u]["type"] == "quantitative":
-        #     load_quantitative_metric(metrics, candidates, u)
 
 
 def load_qualitative_metric(metrics, u):
@@ -156,26 +157,6 @@ def load_quantitative_metric(metrics, u):
             "number" if metrics[u]["displayDecimals"] == 0 else "amount")
     if "lowerIsBetter" not in metrics[u]:
         metrics[u]["lowerIsBetter"] = True
-
-
-# def load_qualitative_metric(metrics, candidates, u):
-#     metrics[u]["max"] = max(c[u] for c in candidates)
-#     metrics[u]["min"] = min(c[u] for c in candidates)
-#     metrics[u]["displayDecimals"] = None
-#     if "lowerIsBetter" not in metrics[u]:
-#         metrics[u]["lowerIsBetter"] = True
-
-
-# def load_quantitative_metric(metrics, candidates, u):
-#     metrics[u]["max"] = max(c[u] for c in candidates)
-#     metrics[u]["min"] = min(c[u] for c in candidates)
-#     metrics[u]["displayDecimals"] = int(metrics[u]["displayDecimals"])
-#     if "countable" not in metrics[u]:
-#         # auto-fill optional field
-#         metrics[u]["countable"] = (
-#             "number" if metrics[u]["displayDecimals"] == 0 else "amount")
-#     if "lowerIsBetter" not in metrics[u]:
-#         metrics[u]["lowerIsBetter"] = True
 
 
 def delete_files(folder):
--- tests/test_nicerange.py
+++ tests/test_nicerange.py
@@ -2,37 +2,34 @@
 Date: 28 Jan 2022
 Author: Cindy Chen """
 from deva.niceRange import nice_range
-import unittest
 
 
-class TestNiceRange(unittest.TestCase):
-    """Unit tests for 'nice_range'."""
-
-    def test_rounded(self):
-        """Test normal rounded case."""
-        self.assertEqual(nice_range(1, 14), (0, 20))
-
-    def test_negative(self):
-        """Test the nice range for negative value."""
-        self.assertEqual(nice_range(-4, 90), (-10, 100))
-
-    def test_zero(self):
-        """Test the nice range for 0."""
-        self.assertEqual(nice_range(0, 0), (-10, 10))
-
-    def test_negative_integer(self):
-        """Test on the negative numbers that can be divided by 10."""
-        self.assertEqual(nice_range(-100, -10), (-110, 0))
-
-    def test_decimals(self):
-        """Test on the non-integers."""
-        self.assertEqual(nice_range(13.9, 52.7), (10, 60))
-
-    def test_wrong_order(self):
-        """Test when the min values and the max values
-           are not correctly ordered in the input."""
-        self.assertEqual(nice_range(122, 69), (60, 130))
+def test_rounded():
+    """Test normal rounded case."""
+    assert(nice_range(1, 14) == (0, 20))
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_negative():
+    """Test the nice range for negative value."""
+    assert(nice_range(-4, 90) == (-10, 100))
+
+
+def test_zero():
+    """Test the nice range for 0."""
+    assert(nice_range(0, 0) == (-10, 10))
+
+
+def test_negative_integer():
+    """Test on the negative numbers that can be divided by 10."""
+    assert(nice_range(-100, -10) == (-110, 0))
+
+
+def test_decimals():
+    """Test on the non-integers."""
+    assert(nice_range(13.9, 52.7) == (10, 60))
+
+
+def test_wrong_order():
+    """Test when the min values and the max values
+        are not correctly ordered in the input."""
+    assert(nice_range(122, 69) == (60, 130))
