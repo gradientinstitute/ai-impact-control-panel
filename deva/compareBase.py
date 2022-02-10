edff1cddfe265a19c66c03774c920126508e25cf
Cindy Chen <u6827101@anu.edu.au>
2022-02-10 12:53:34 +0800

    report

--- deva/compareBase.py
+++ deva/compareBase.py
@@ -1,42 +1,53 @@
-# TODO  for each REFERENCE (eg inaction, industry avg), report whether they meet the current bounds,
-# 	 	and if they don't, identify why not (eg "inaction: true positive rate is too low")
+"""
+Generate the report shown in the frontend.
+
+Date: Feb 10, 2022
+Author: Muyao Chen
+"""
 
 
 def compare(meta, baseline, bounds):
     """
-    baseline ={
-        "inaction": {metrics: value}
-        "industry_average": {metrics: value}
+    Compare baselines against the boundaries.
+
+    For each reference, report whether they meet the current bounds,
+    and if they don't, identify why not.
+
+    Parameters
+    ----------
+    baseline: dict
+    {
+      "inaction": {metrics: value}
+      "industry_average": {metrics: value}
     }
 
-    bounds = {metrics: [min,max]}
-    """
+    bounds: dict
+    {metrics: [min_value, max_value]}
 
+    Returns
+    -------
+    report: string
+
+    """
     report = ""
     metrics = meta["metrics"]
 
-    for ref in baseline:
-        for m in baseline[ref]:
-            val = bounds[m]
-            refVal = baseline[ref][m]
-
-            if metrics[m]["type"] == "quantitative":
+    for m in bounds:
+        if metrics[m]["type"] == "quantitative":
+            report += m + "\n"
+            for ref in baseline:
+                val = bounds[m]
+                refVal = baseline[ref][m]
                 lowerIsBetter = metrics[m]["lowerIsBetter"]
-                report += m + "\n"
+
                 report += ref + ": " + metrics[m]["description"][:-1]
-                
-                if val[1] < refVal:  # reference not in range 
-                    # report += f"{ref}: {}"
-                    
+
+                if val[1] < refVal:  # reference not in range
                     if lowerIsBetter:
-                        report += " is too high\n"
+                        report += " is too high.\n"
                     else:
-                        report += " is too low\n"
+                        report += " is too low.\n"
                 else:
-                    report += " is within the boundaries\n"
-            # else:
-            #     if val[1] > refVal:
-            #         # baselines in range 
-            #         pass 
+                    report += " is within the boundaries.\n"
 
     return report
