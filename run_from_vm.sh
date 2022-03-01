c47bdf374d810d8ac006c6f34f9fce446fd7e435
Lachlan McCalman <lachlan@mccalman.info>
2022-03-01 14:49:42 +1100

    tweak vm runfile

--- run_from_vm.sh
+++ run_from_vm.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-git checkout demo
 docker kill $(docker ps -q)
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w="$PWD" docker/compose:1.29.0 -f docker-compose.yml -f docker-compose-prod.yml up -d
