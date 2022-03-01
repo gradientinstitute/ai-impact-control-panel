b8c95fcf955c4aabf349f7dbb817a6edba641f01
Lachlan McCalman <lachlan@mccalman.info>
2022-03-01 15:15:21 +1100

    use docker-compose down

--- source_on_vm.sh
+++ source_on_vm.sh
@@ -4,5 +4,5 @@
 # assuming we're running docker-compose in docker (see sock mapping)
 #  https://cloud.google.com/community/tutorials/docker-compose-on-container-optimized-os
 
-docker kill $(docker ps -q)
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w="$PWD" docker/compose:1.29.0 down --rmi all
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w="$PWD" docker/compose:1.29.0 -f docker-compose-prod.yml up -d
