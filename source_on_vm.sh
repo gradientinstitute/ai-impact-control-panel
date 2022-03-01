415ec9bb6c8a07bcb087bdb668fb17864a8f9bf9
Lachlan McCalman <lachlan@mccalman.info>
2022-03-01 15:09:12 +1100

    only prod dockercompose

--- source_on_vm.sh
+++ source_on_vm.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Kills all the current containers, spins up new ones based on latest images
+# assuming we're running docker-compose in docker (see sock mapping)
+#  https://cloud.google.com/community/tutorials/docker-compose-on-container-optimized-os
+
 docker kill $(docker ps -q)
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w="$PWD" docker/compose:1.29.0 -f docker-compose.yml -f docker-compose-prod.yml up -d
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w="$PWD" docker/compose:1.29.0 -f docker-compose-prod.yml up -d
